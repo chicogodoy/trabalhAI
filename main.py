385760f85c22278c5de5d349bb3a7aae7795519a
Francisco Godoy Pereira <chicogodoy.evo@gmail.com>
2026-04-30 20:18:22 -0300

    Configuração inicial do projeto

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def get_data():
+    return {
+        "titulo": "Meu Resultado",
+        "mensagem": "Olá, mundo!",
+        "data": "29 de abril de 2026",
+        "itens": ["Item 1", "Item 2", "Item 3", "Aqui é só na Preview!"]
+    }
+
+@app.route("/")
+def index():
+    dados = get_data()
+    return render_template("index.html", **dados)
+
+if __name__ == "__main__":
+    app.run(debug=True, extra_files=['templates/index.html'])
